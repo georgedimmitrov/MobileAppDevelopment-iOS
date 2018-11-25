c9daa24bc0b16aad8f7aa286fcb15e08bc8fc66b
Georgi Dimitrov <dgeorgi@vmware.com>
2018-11-25 14:07:28 +0200

    Initial commit test if gitignore is ok

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Georgi Dimitrov on 25.11.18.
+//  Copyright © 2018 Georgi Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
