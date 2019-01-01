f8554c7b6aa300fc8d8a3babbb280ca0213590df
Georgi Dimitrov <dgeorgi@vmware.com>
2019-01-01 11:20:28 +0200

    Convert to MVC Design Pattern

---
+++ Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Georgi Dimitrov on 1.01.19.
+//  Copyright © 2019 Georgi Dimitrov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
